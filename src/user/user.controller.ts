124effb545ce1412e87a298608ce43c0621aa08a
Oleg Matus <matys_bboy96@ukr.net>
2023-10-29 20:49:00 +0200

    HW_1&Connect_DB_postgres

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,42 @@
-import {Controller} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+
+import { UserCreateDto } from './dto/user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get('/list')
+  async getAllUsers() {
+    return await this.userService.getAllUsers();
+  }
+
+  @Get('/:userId')
+  async getById(@Param() param: { userId: string }) {
+    return await this.userService.getOneUser(param.userId);
+  }
+
+  @Post('create')
+  async createUser(@Body() body: UserCreateDto, @Res() res: any) {
+    // return await this.userService.createUser(body);
+    return res
+      .status(HttpStatus.CREATED)
+      .json(this.userService.createUser(body));
+  }
+
+  @Patch('/:userId')
+  async updateUser() {}
+
+  @Delete('/:userId')
+  async deleteUser() {}
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { UserCreateDto } from './dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users = [];
+
+  constructor() {}
+
+  async createUser(userData: UserCreateDto) {
+    if (userData.age < 18) {
+      throw new HttpException(
+        'User is not yet 18 years old',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const id = uuid();
+    const newUser = { id, userData };
+    this.users.push(newUser);
+    return this.users;
+  }
+  async getOneUser(userId: string) {
+    return this.users.find((item) => item.id === Number(userId))[0];
+  }
+  async getAllUsers() {
+    return this.users;
+  }
+}
